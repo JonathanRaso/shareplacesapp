7e5d70e3964a16d254845253387cbe8a6ae2e86c
JonathanRaso <jonathanraso.dev@gmail.com>
2020-10-12 10:37:03 +0200

    UsersList and UserItem components added

--- share-a-place-front/src/user/pages/Users.js
+++ share-a-place-front/src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-  return <h2>Users Works!</h2>;
+  const USERS = [
+    {
+      id: 'u1', 
+      name: 'John Test', 
+      image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.V0lOa_0dCA2k2dvZHSZeNwHaHv%26pid%3DApi&f=1',
+      places: 3
+    }
+  ];
+
+  return <UsersList items={USERS}/>;
 };
 
 export default Users;
