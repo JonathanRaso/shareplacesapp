a0440de03edc6464b6dc87f1271130623e1baa66
JonathanRaso <jonathanraso.dev@gmail.com>
2020-10-19 15:11:49 +0200

    Error handling middleware and http-error model added

--- share-a-place-back/app.js
+++ share-a-place-back/app.js
@@ -7,4 +7,13 @@ const app = express();
 
 app.use('/api/places', placesRoutes);
 
+// Error handling middleware function. Will only be executed on requests that have an error attached to it
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || 'An unknown error occured!' });
+});
+
 app.listen(5000);
--- share-a-place-back/routes/places-routes.js
+++ share-a-place-back/routes/places-routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const HttpError = require('../models/http-error');
+
 const router = express.Router();
 
 const DUMMY_PLACES = [
@@ -19,12 +21,28 @@ const DUMMY_PLACES = [
 router.get('/:pid', (req, res, next) => {
   const placeId = req.params.pid;
   const place = DUMMY_PLACES.find((place) => place.id === placeId);
+
+  if (!place) {
+    // We can use throw or next() in a sync function. But we need to use next() if we are in async function
+    // This error will trigger our error handling middleware
+    // With throw, we don't have to return because it already cancels the function execution
+    throw new HttpError('Could not find a place for the provided id.', 404);
+  }
+
   res.json({ place }); // => { place: place }
 });
 
 router.get('/user/:uid', (req, res, next) => {
   const userId = req.params.uid;
   const place = DUMMY_PLACES.find((place) => place.creator === userId);
+
+  if (!place) {
+    // We can use throw or next() in a sync function. But we need to use next() if we are in async function
+    // This error will trigger our error handling middleware
+    // Don't forget to return, or the other response will be sent and it's not possible to send 2 responses
+    return next(new HttpError('Could not find a place for the provided user id.', 404));
+  }
+
   res.json({ place }); // => { place: place }
 });
 
