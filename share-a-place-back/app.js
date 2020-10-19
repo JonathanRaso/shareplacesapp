1c916771bce94be9d42ac22c4312c6711047776d
JonathanRaso <jonathanraso.dev@gmail.com>
2020-10-19 11:30:02 +0200

    Basic node app and routing added

--- share-a-place-back/app.js
+++ share-a-place-back/app.js
@@ -1,1 +1,11 @@
-console.log('test');
+const express = require('express');
+const bodyParser = require('body-parser');
+const { urlencoded } = require('body-parser');
+
+const placesRoutes = require('./routes/places-routes');
+
+const app = express();
+
+app.use(placesRoutes);
+
+app.listen(5000);
