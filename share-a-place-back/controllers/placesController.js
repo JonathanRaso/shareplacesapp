8a0cc706d9916ae35bd98fc7e76e126a36e879f5
JonathanRaso <jonathanraso.dev@gmail.com>
2020-10-23 15:45:34 +0200

    Methods for getting place by id and by user id, using mongoose, added

--- share-a-place-back/controllers/placesController.js
+++ share-a-place-back/controllers/placesController.js
@@ -19,32 +19,49 @@ let DUMMY_PLACES = [
   }
 ];
 
-const getPlaceById = (req, res, next) => {
+const getPlaceById = async (req, res, next) => {
   const placeId = req.params.pid;
-  const place = DUMMY_PLACES.find((place) => place.id === placeId);
+  let place; 
+
+  try {
+    place = await Place.findById(placeId);
+  } catch (err) {
+    const error = new HttpError('Something went wrong, could not find a place.', 500);
+    return next(error);
+  }
 
   if (!place) {
     // We can use throw or next() in a sync function. But we need to use next() if we are in async function
     // This error will trigger our error handling middleware
     // With throw, we don't have to return because it already cancels the function execution
-    throw new HttpError('Could not find a place for the provided id.', 404);
+    const error = new HttpError('Could not find a place for the provided id.', 404);
+    return next(error);
   }
 
-  res.json({ place }); // => { place: place }
+  res.json({ place: place.toObject( { getters: true } ) }); // => { place: place } / { getters: true } => used to get rid of the '_' with _id. Mongoose add an id property to the created object
 };
 
-const getPlacesByUserId = (req, res, next) => {
+const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
-  const places = DUMMY_PLACES.filter((place) => place.creator === userId);
+  
+  let places;
+
+  try {
+    places = await Place.find({ creator: userId });
+  } catch (err) {
+    const error = new HttpError('Fetching places failed, please try again later.', 500);
+    return next(error);
+  }
 
   if (!places || places.length === 0) {
     // We can use throw or next() in a sync function. But we need to use next() if we are in async function
     // This error will trigger our error handling middleware
     // Don't forget to return, or the other response will be sent and it's not possible to send 2 responses
-    return next(new HttpError('Could not find places for the provided user id.', 404));
+    const error = new HttpError('Could not find places for the provided user id.', 404);
+    return next(error);
   }
 
-  res.json({ places }); // => { place: place }
+  res.json({ places: places.map((place) => place.toObject( { getters: true } )) }); // it's an array here, so we have to map on it, and not use toObject like in the getPlacesById method /{ getters: true } => used to get rid of the '_' with _id. Mongoose add an id property to the created object
 };
 
 const createPlace = async (req, res, next) => {
