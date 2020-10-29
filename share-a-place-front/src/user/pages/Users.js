eaf0ed8f9623e3496aab959d6d4317230f3a37bb
JonathanRaso <jonathanraso.dev@gmail.com>
2020-10-29 11:23:00 +0100

    GET request for getting users from database added. Changed placeCount inside UsersList.js because we receive an array now

--- share-a-place-front/src/user/pages/Users.js
+++ share-a-place-front/src/user/pages/Users.js
@@ -1,18 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import UsersList from '../components/UsersList';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 
 const Users = () => {
-  const USERS = [
-    {
-      id: 'u1', 
-      name: 'John Test', 
-      image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.V0lOa_0dCA2k2dvZHSZeNwHaHv%26pid%3DApi&f=1',
-      places: 3
-    }
-  ];
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [loadedUsers, setLoadedUsers] = useState();
 
-  return <UsersList items={USERS}/>;
+  // Don't use async with useEffect hook, create a function inside useEffect if you need async/await
+  useEffect(() => {
+    const sendRequest = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch('http://localhost:5000/api/users');
+  
+        const responseData = await response.json();
+  
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        setLoadedUsers(responseData.users);
+      } catch (err) {
+        setError(err.message);
+      }
+      setIsLoading(false);
+    };
+    sendRequest();
+  }, []);
+
+  const errorHandler = () => {
+    setError(null);
+  }
+
+  return (
+    <>
+    <ErrorModal error={error} onClear={errorHandler} />
+    {isLoading && (
+      <div className="center">
+        <LoadingSpinner />
+      </div>
+    )}
+    {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+    </>
+  )
 };
 
 export default Users;
