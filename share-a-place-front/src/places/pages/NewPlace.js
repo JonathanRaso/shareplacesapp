e4e4d23072b08e2312c6d04ca2ee4c2b0e274d08
JonathanRaso <jonathanraso.dev@gmail.com>
2020-10-30 14:39:13 +0100

    Upload image for new place, added

--- share-a-place-front/src/places/pages/NewPlace.js
+++ share-a-place-front/src/places/pages/NewPlace.js
@@ -4,7 +4,9 @@ import { useHistory } from 'react-router-dom';
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
 import ErrorModal from '../../shared/components/UIElements/ErrorModal';
-import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner'
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import ImageUpload from '../../shared/components/FormElements/ImageUpload';
+
 import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from '../../shared/util/validators';
 import { useForm } from '../../shared/hooks/form-hook';
 import { useHttpClient } from '../../shared/hooks/http-hook';
@@ -27,6 +29,10 @@ const NewPlace = () => {
       address: {
         value: '',
         isValid: false
+      },
+      image: {
+        value: null,
+        isValid: false
       }
     },
     false
@@ -37,18 +43,19 @@ const NewPlace = () => {
 
   const placeSubmitHandler = async (event) => {
     event.preventDefault();
+    
+    const formData = new FormData();
+    formData.append('title', formState.inputs.title.value);
+    formData.append('description', formState.inputs.description.value);
+    formData.append('address', formState.inputs.address.value);
+    formData.append('creator', auth.userId);
+    formData.append('image', formState.inputs.image.value);
 
     try {
       await sendRequest(
         'http://localhost:5000/api/places',
         'POST',
-        JSON.stringify({
-          title: formState.inputs.title.value,
-          description: formState.inputs.description.value,
-          address: formState.inputs.address.value,
-          creator: auth.userId,
-        }),
-        { 'Content-Type': 'application/json' }
+        formData
       );
       // Redirect the user to a different page
       history.push('/');
@@ -85,6 +92,7 @@ const NewPlace = () => {
         errorText="Please enter a valid address."
         onInput={inputHandler} 
       />
+      <ImageUpload id="image" onInput={inputHandler} errorText="Please provide an image" />
       <Button type="submit" disabled={!formState.isValid}>
         ADD PLACE  
       </Button>
