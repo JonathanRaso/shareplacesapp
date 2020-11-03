e2617539f98ca6caa16d0e3496211a31d2efd82b
JonathanRaso <jonathanraso.dev@gmail.com>
2020-11-03 10:45:01 +0100

    Custom hook (auth-hook) for managing login/logout/authentication to get the logic out of the App component added

--- share-a-place-front/src/App.js
+++ share-a-place-front/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
 import Users from'./user/pages/Users';
@@ -8,64 +8,12 @@ import UpdatePlace from './places/pages/UpdatePlace';
 import PageAuth from './user/pages/PageAuth';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import { AuthContext } from './shared/context/auth-context';
+import { useAuth } from './shared/hooks/auth-hook';
 
-let logoutTimer;
+
 
 const App = () => {
-  const [token, setToken] = useState(false);
-  const [tokenExpirationDate, setTokenExpirationDate] = useState();
-  const [userId, setUserId] = useState(false);
-
-  
-  const login = useCallback((uid, token, expirationDate) => {
-    setToken(token);
-    setUserId(uid);
-    // Generate a new object date that's based on the current time + 1 hour. Our token will expire 1h after creation
-    // If we already have an expirationDate for the token, we keep it. Else, we create a new one.
-    // We need that because when we refresh the page, it would generate a new 1h token and we don't want that
-    const tokenDurationDate = expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
-    setTokenExpirationDate(tokenDurationDate);
-    localStorage.setItem(
-      'userData',
-      JSON.stringify({ 
-        userId: uid,
-        token: token, 
-        expiration: tokenDurationDate.toISOString() 
-      })
-    );
-    }, []);
-    
-    const logout = useCallback(() => {
-      setToken(null);
-      setTokenExpirationDate(null);
-      setUserId(null);
-      localStorage.removeItem('userData');
-    }, []);
-
-    // Here, if the token changes, we want to work with our timer
-    useEffect(() => {
-      if (token && tokenExpirationDate) {
-        const remainingTime = tokenExpirationDate.getTime() - new Date().getTime();
-        logoutTimer = setTimeout(logout, remainingTime);
-      } else {
-        clearTimeout(logoutTimer);
-      }
-    }, [token, logout, tokenExpirationDate]);
-    
-    // useEffect with empty array [] will only runs once. It also runs after the render cycle
-    // Here, there is a dependency (login), but thanks to useCallback(uid, token), login will run only once
-    useEffect(() => {
-      // JSON.parse converts JSON strings back to regular javascript data structures (like object)
-      const storedData = JSON.parse(localStorage.getItem('userData'));
-      if (
-        storedData && 
-        storedData.token && 
-        // We check if the expiration date in our token is greater than the actual time. If yes, the token is still valid
-        new Date(storedData.expiration) > new Date()
-      ) {
-        login(storedData.userId, storedData.token, new Date(storedData.expiration));
-      }
-    }, [login]);
+  const { token, login, logout, userId } = useAuth();  
 
     let routes;
     
